7e68dfa8d8fcc8e92ef3f959f3a67d197d813dc2
Rakesh-Reddy-Bollapally <rakeshreddybollapally@gmail.com>
2025-06-13 22:42:19 +0530

    Update script1.js

--- script1.js
+++ script1.js
@@ -640,7 +640,7 @@ products.forEach(function(v,i,ar) {
     div.style.width="500px";
     div.style.border="2px solid black";
     div.style.margin="0px auto";
-    div.style.marginTop="15px"
+    div.style.marginTop="20px"
     div.style.borderRadius="5px"
     div.style.padding="10px"
     div.style.fontFamily="calibri";
